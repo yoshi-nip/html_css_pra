cb797e94920a1e20123c93a7fd7118b8b2ecc3ed
yoshi <mdmdmdmdmd57@gmail.com>
2023-06-12 08:59:42 +0900

    inputの情報が取得できるか確認

--- javascript/todo_list/javascript/main.js
+++ javascript/todo_list/javascript/main.js
@@ -1,5 +1,7 @@
 const onclick = () => {
-  alert();
+  const addText = window.document.getElementById('add-text').value;
+  window.document.getElementById('add-text').value = '';
+  alert(addText);
 };
 
 window.document.getElementById('add-button').addEventListener('click', onclick);
