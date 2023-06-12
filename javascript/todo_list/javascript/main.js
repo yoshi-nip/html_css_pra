6a2df5d8bb3bc80c7bab0668fdb4c475fceba273
yoshi <mdmdmdmdmd57@gmail.com>
2023-06-12 09:23:00 +0900

    追加ボタンを押した際、input情報を入れたDOMが作成されるようにする

--- javascript/todo_list/javascript/main.js
+++ javascript/todo_list/javascript/main.js
@@ -3,9 +3,40 @@ const onclick = () => {
   window.document.getElementById('add-text').value = '';
   alert(addText);
 
+  const li = document.createElement('li');
+
   const div = document.createElement('div');
-  div.className = 'テストネーム';
+  div.className = 'li-wrapper';
   console.log(div);
+
+  const p = document.createElement('p');
+  p.innerText = addText;
+
+  const completeButton = document.createElement('button');
+  completeButton.className = 'complete-button';
+  completeButton.innerText = '完了';
+  console.log(completeButton);
+
+  const deleteButton = document.createElement('button');
+  deleteButton.className = 'delete-button';
+  deleteButton.innerText = '削除';
+  console.log(deleteButton);
+
+  div.appendChild(p);
+  div.appendChild(completeButton);
+  div.appendChild(deleteButton);
+
+  li.appendChild(div);
+  console.log(li);
+
+  document.getElementById('incomplete-list').appendChild(li);
+  // <li>
+  //         <div class="li-wrapper">
+  //           <p>ToDoです</p>
+  //           <button>完了</button>
+  //           <button>削除</button>
+  //         </div>
+  //       </li>
 };
 
 window.document.getElementById('add-button').addEventListener('click', onclick);
