13b277d351a943d621cb96f3cad62507a6abc86e
yoshi <mdmdmdmdmd57@gmail.com>
2023-06-12 09:03:31 +0900

    DOM生成の確認

--- javascript/todo_list/javascript/main.js
+++ javascript/todo_list/javascript/main.js
@@ -2,6 +2,10 @@ const onclick = () => {
   const addText = window.document.getElementById('add-text').value;
   window.document.getElementById('add-text').value = '';
   alert(addText);
+
+  const div = document.createElement('div');
+  div.className = 'テストネーム';
+  console.log(div);
 };
 
 window.document.getElementById('add-button').addEventListener('click', onclick);
