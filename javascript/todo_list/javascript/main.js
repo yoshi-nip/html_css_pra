bdf4e9bbb815d3dd25e4400ee8ecab693d994853
yoshi <mdmdmdmdmd57@gmail.com>
2023-06-14 18:53:28 +0900

    共通化できるものを共通化し、動きを修正

--- javascript/todo_list/javascript/main.js
+++ javascript/todo_list/javascript/main.js
@@ -2,13 +2,26 @@ const onclick = () => {
   const addText = window.document.getElementById('add-text').value;
   window.document.getElementById('add-text').value = '';
   alert(addText);
+  createIncompleteList(addText);
+};
 
+// 未完了から消す挙動
+const deleteFromIncompleteList = (target) => {
+  document.getElementById('incomplete-list').removeChild(target);
+};
+// 完了から消す挙動
+const backFromIncompleteList = (target) => {
+  document.getElementById('complete-list').removeChild(target);
+};
+
+// 未完了リスト作成の動きを共通化
+const createIncompleteList = (text) => {
   //要素の作成
   const li = document.createElement('li');
   const div = document.createElement('div');
   div.className = 'li-wrapper';
   const p = document.createElement('p');
-  p.innerText = addText;
+  p.innerText = text;
   const completeButton = document.createElement('button');
   completeButton.className = 'complete-button';
   completeButton.innerText = '完了';
@@ -31,7 +44,6 @@ const onclick = () => {
     const addTargetDiv = addTarget.firstElementChild;
     const addText = addTarget.firstElementChild.firstElementChild.innerText;
     deleteFromIncompleteList(completeButton.parentNode.parentNode);
-    // console.log(comleteNode)
 
     //初期化して要素を再度作成
     addTargetDiv.textContent = null;
@@ -51,29 +63,9 @@ const onclick = () => {
     //戻すボタンを押した時
     backButton.addEventListener('click', () => {
       const backTarget = backButton.parentNode.parentNode;
-      const backTargetDiv = backTarget.firstElementChild;
       const addText = backTarget.firstElementChild.firstElementChild.innerText;
-      backFromIncompleteList(backButton.parentNode.parentNode);
-      //初期化して要素を再度作成
-      backTargetDiv.textContent = null;
-
-      const p = document.createElement('p');
-      const completeButton = document.createElement('button');
-      completeButton.className = 'complete-button';
-      completeButton.innerText = '完了';
-
-      const deleteButton = document.createElement('button');
-      deleteButton.className = 'delete-button';
-      deleteButton.innerText = '削除';
-
-      p.innerText = addText;
-      addTargetDiv.appendChild(p);
-      addTargetDiv.appendChild(completeButton);
-      addTargetDiv.appendChild(deleteButton);
-      console.log(backTarget);
-
-      //未完了リストに入れる
-      document.getElementById('incomplete-list').appendChild(backTarget);
+      backFromIncompleteList(backTarget);
+      createIncompleteList(addText);
     });
   });
 
@@ -82,39 +74,5 @@ const onclick = () => {
     deleteFromIncompleteList(deleteButton.parentNode.parentNode);
   });
 };
-
-const deleteFromIncompleteList = (target) => {
-  // const deleteTarget = deleteButton.parentNode.parentNode;
-  document.getElementById('incomplete-list').removeChild(target);
-};
-const backFromIncompleteList = (target) => {
-  // const deleteTarget = deleteButton.parentNode.parentNode;
-  document.getElementById('complete-list').removeChild(target);
-};
-
-//完了ボタンを押した時
-  completeButton.addEventListener('click', () => {
-    // 追加するターゲットを追うのと、削除
-    const addTarget = completeButton.parentNode.parentNode;
-    const addTargetDiv = addTarget.firstElementChild;
-    const addText = addTarget.firstElementChild.firstElementChild.innerText;
-    deleteFromIncompleteList(completeButton.parentNode.parentNode);
-    // console.log(comleteNode)
-
-    //初期化して要素を再度作成
-    addTargetDiv.textContent = null;
-
-    const p = document.createElement('p');
-    const backButton = document.createElement('button');
-    backButton.className = 'back-button';
-
-    p.innerText = addText;
-    backButton.innerText = '戻す';
-    addTargetDiv.appendChild(p);
-    addTargetDiv.appendChild(backButton);
-
-    //完了リストに入れる
-    document.getElementById('complete-list').appendChild(addTarget);
-
 
 window.document.getElementById('add-button').addEventListener('click', onclick);
--- javascript/todo_list/javascript/module.js
+++ javascript/todo_list/javascript/module.js
@@ -1,29 +1,78 @@
-export class Robot {
-  constructor(color) {
-    this.color = color;
-  }
+//要素の作成
+const li = document.createElement('li');
+const div = document.createElement('div');
+div.className = 'li-wrapper';
+const p = document.createElement('p');
+p.innerText = addText;
+const completeButton = document.createElement('button');
+completeButton.className = 'complete-button';
+completeButton.innerText = '完了';
+const deleteButton = document.createElement('button');
+deleteButton.className = 'delete-button';
+deleteButton.innerText = '削除';
 
-  sayHello() {
-    console.log('Hello');
-  }
-}
+//要素を階層ごとに挿入
+div.appendChild(p);
+div.appendChild(completeButton);
+div.appendChild(deleteButton);
+li.appendChild(div);
 
-export const robot = new Robot('red');
+document.getElementById('incomplete-list').appendChild(li);
 
-robot.color = 'blue';
+//完了ボタンを押した時
+completeButton.addEventListener('click', () => {
+  // 追加するターゲットを追うのと、削除
+  const addTarget = completeButton.parentNode.parentNode;
+  const addTargetDiv = addTarget.firstElementChild;
+  const addText = addTarget.firstElementChild.firstElementChild.innerText;
+  deleteFromIncompleteList(completeButton.parentNode.parentNode);
+  // console.log(comleteNode)
 
-export default class Power {
-  on() {
-    console.log('電源をつける');
-  }
-}
-class StrongPower extends Power {
-  on() {
-    console.log('強い電源をつける');
-  }
-}
-const power = new Power();
+  //初期化して要素を再度作成
+  addTargetDiv.textContent = null;
 
-export const strongPower = new StrongPower();
-power.on();
-strongPower.on();
+  const p = document.createElement('p');
+  const backButton = document.createElement('button');
+  backButton.className = 'back-button';
+
+  p.innerText = addText;
+  backButton.innerText = '戻す';
+  addTargetDiv.appendChild(p);
+  addTargetDiv.appendChild(backButton);
+
+  //完了リストに入れる
+  document.getElementById('complete-list').appendChild(addTarget);
+
+  //戻すボタンを押した時
+  backButton.addEventListener('click', () => {
+    const backTarget = backButton.parentNode.parentNode;
+    const backTargetDiv = backTarget.firstElementChild;
+    const addText = backTarget.firstElementChild.firstElementChild.innerText;
+    backFromIncompleteList(backButton.parentNode.parentNode);
+    //初期化して要素を再度作成
+    backTargetDiv.textContent = null;
+
+    const p = document.createElement('p');
+    const completeButton = document.createElement('button');
+    completeButton.className = 'complete-button';
+    completeButton.innerText = '完了';
+
+    const deleteButton = document.createElement('button');
+    deleteButton.className = 'delete-button';
+    deleteButton.innerText = '削除';
+
+    p.innerText = addText;
+    addTargetDiv.appendChild(p);
+    addTargetDiv.appendChild(completeButton);
+    addTargetDiv.appendChild(deleteButton);
+    console.log(backTarget);
+
+    //未完了リストに入れる
+    document.getElementById('incomplete-list').appendChild(backTarget);
+  });
+});
+
+//削除ボタンを押したとき
+deleteButton.addEventListener('click', () => {
+  deleteFromIncompleteList(deleteButton.parentNode.parentNode);
+});
